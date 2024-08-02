6df34abc61d92bc5941c48c3cd23ab74c09beeb4
Hasibur013 <hasibur01631753881@gmail.com>
2024-08-02 20:36:03 +0600

    serializer done

--- apiproject/myapp/views.py
+++ apiproject/myapp/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from myapp.models import Contact
+from myapp.serializers import ContactSerializer
+
+@csrf_exempt
+def api_list(request):
+    if request.method == 'GET':
+        apivar = Contact.objects.all()
+        serializer = ContactSerializer(apivar, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ContactSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+@csrf_exempt
+def api_detail(request, pk):
+    try:
+        detailVar = Contact.objects.get(pk=pk)
+    except Contact.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ContactSerializer(detailVar)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ContactSerializer(detailVar, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        detailVar.delete()
+        return HttpResponse(status=204)
